e4bd85fc8763fad4103df32a04d78a821bc4c706
Daniel Bastos <daniel.bastos1710@gmail.com>
2017-10-01 16:51:59 -0300

    Populate view.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,14 @@ from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
+
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
     db.init_app(app)
-
-    @app.route('/')
-    def root():
-        return 'Hello, World!'
-
     migrate = Migrate(app, db)
-
     from app import models
+    from app import views
 
     return app
