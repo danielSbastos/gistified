89136173036964f0ca3cef360d1ce27017f52609
Daniel Bastos <daniel.bastos1710@gmail.com>
2017-10-07 11:14:36 -0300

    Isolate tests common configuration

--- tests/app/test_views.py
+++ tests/app/test_views.py
@@ -1,37 +1,9 @@
-import os
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_testing import TestCase
-import unittest
-
-from app import create_app, db
+from ..conf import ApplicationTestCase
 from app.models import Gist
+from app import db
 
 
-class TestViews(TestCase):
-
-    def create_app(self):
-        app = create_app()
-        if os.getenv('CIRCLECI'):
-            database_uri = os.getenv('SQLALCHEMY_DATABASE_URI')
-        else:
-            database_uri = 'postgresql:///gistified_test'
-
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
-        db.init_app(app)
-        return app
-
-    def setUp(self):
-        db.session.commit()
-        db.drop_all()
-        db.create_all()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+class TestViews(ApplicationTestCase):
 
     def test_home(self):
         response = self.client.get('/')
