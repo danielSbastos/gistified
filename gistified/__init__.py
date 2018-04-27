6eaa687f54693e6cb99d9cf8aeadb136b682275d
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-04-27 17:29:56 -0300

    test for feito

--- gistified/__init__.py
+++ gistified/__init__.py
@@ -1,12 +1,12 @@
 import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+    from flask_migrate import
+    Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql:///gistified')
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql:///gistified')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
